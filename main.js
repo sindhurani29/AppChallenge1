fa4f23b86535ae5233869cd4eaeaf603cf3ab844
sindhu rani goli <saisindhurani@gmail.com>
2020-03-02 11:33:21 -0600

    commit

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 function myFunction() {
-   
 
-   string= maxLength - sentence.length
+
+   maxLength=250;
+   str1= maxLength - sentence.length;
+
+  
+     r = document.getElementById("string1").value;
+   
+   document.getElementById("check").innerHTML = r;
+ 
+
+
  }
